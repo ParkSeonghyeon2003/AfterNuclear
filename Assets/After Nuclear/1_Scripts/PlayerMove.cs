a38591937c724b8f299c9e3a56008c6795f4b739
박성현 <shpark8267@gmail.com>
2024-12-11 01:35:37 +0900

    플레이어 이동 및 애니메이션

--- Assets/After Nuclear/1_Scripts/PlayerMove.cs
+++ Assets/After Nuclear/1_Scripts/PlayerMove.cs
@@ -1,34 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    Vector2 inputVec;
     Rigidbody2D rigid;
-    public float maxSpeed;
+    SpriteRenderer spriter;
+    Animator anim;
+
+    public float maxSpeed; // 최대 속도
+    public float jumpPower; // 점프 파워
+
+    float horizontal;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("IsJumping"))
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("IsJumping", true);
+        }
+
         if (Input.GetButtonUp("Horizontal"))
         {
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
         }
+
+        if (horizontal != 0.0f)
+        {
+            spriter.flipX = horizontal > 0;
+        }
+
+        if (Mathf.Abs(rigid.velocity.x) < 0.3f || anim.GetBool("IsJumping"))
+            anim.SetBool("IsWalking", false);
+        else
+            anim.SetBool("IsWalking", true);
     }
 
     void FixedUpdate()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        horizontal = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * horizontal, ForceMode2D.Impulse);
         if (rigid.velocity.x > maxSpeed)
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         if (rigid.velocity.x < maxSpeed * (-1))
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+        Debug.DrawRay(rigid.position, Vector2.down * 2, new Color(1, 0, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector2.down, 1.2f, LayerMask.GetMask("Platform"));
+        if (rigid.velocity.y < 0.0f)
+        {
+            if (rayHit.collider != null)
+            {
+                anim.SetBool("IsJumping", false);
+            }
+        }
     }
 }
